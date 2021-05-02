97b91721c66d838e414e3a7eb77f583ad382c494
jwinternet <47834941+Killface0@users.noreply.github.com>
2021-05-01 22:30:11 -0400

    Made a few tweaks, tried to use the datetime library to name the files by date, will need further work.

--- file_renamer.py
+++ file_renamer.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 """File Renamer Program"""
 
+# from datetime import datetime
 import os
 
 
 def file_renamer():
     path = input("What is the folder/directory location of the files? ")
-    file_names = input("What should the beginning of each file be called? ")
+    start_of_name = input("What should the beginning of each file be called? ")
     start_number = input("What number should the files start being numbered at? ")
     file_number = int(start_number)
 
-    for filename in os.listdir(path):
-        if filename.lower().endswith(".png"):
-            os.rename(os.path.join(path,filename), os.path.join(path, file_names + str(start_number)+'.png'))
+    for file_name in os.listdir(path):
+        # date = datetime.fromtimestamp(os.path.getctime(file_name)).strftime('%m_%d_%Y-%H_%M_%S')
+        if file_name.lower().endswith(".png"):
+            os.rename(os.path.join(path,file_name), os.path.join(path, start_of_name + file_number + ".png"))
             file_number = file_number + 1
-        elif filename.lower().endswith(".jpg"):
-            os.rename(os.path.join(path,filename), os.path.join(path, file_names + str(start_number)+'.jpg'))
+        elif file_name.lower().endswith(".jpg"):
+            os.rename(os.path.join(path,file_name), os.path.join(path, start_of_name + file_number + ".jpg"))
             file_number = file_number + 1
-        elif filename.lower().endswith(".jpeg"):
-            os.rename(os.path.join(path,filename), os.path.join(path, file_names + str(start_number)+'.jpeg'))
+        elif file_name.lower().endswith(".jpeg"):
+            os.rename(os.path.join(path,file_name), os.path.join(path, start_of_name + file_number + ".jpeg"))
             file_number = file_number + 1
-        elif filename.lower().endswith(".mp4"):
+        elif file_name.lower().endswith(".mp4"):
             pass
 
 
